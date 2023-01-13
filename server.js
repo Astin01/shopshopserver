aa55b5b2cfc878174bcf415268536118d68f6895
astin <wonhun1225@gmail.com>
2023-01-13 20:45:45 +0900

    make signin , login

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
 app.use(express.json());
 var cors = require("cors");
 app.use(cors());
+
+app.use(
+  session({ secret: "비밀코드", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // app.use(express.static(path.join(__dirname, "react-project/build")));
 
@@ -30,6 +39,47 @@ app.post("/signin", function (req, res) {
     }
   );
   res.send("전송완료");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/fail" }),
+  function (req, res) {
+    res.redirect("/");
+    console.log("성공");
+  }
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "id",
+      passwordField: "pw",
+      session: true,
+      passReqToCallback: false,
+    },
+    function (inputId, inputPw, done) {
+      //console.log(inputId, inputPw);
+      db.collection("login").findOne({ id: inputId }, function (err, result) {
+        if (err) return done(err);
+        if (!result)
+          return done(null, false, { message: "존재하지않는 아이디입니다" });
+        if (inputPw == result.pw) {
+          return done(null, result);
+        } else {
+          return done(null, false, { message: "비번이 틀렸습니다" });
+        }
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  done(null, {});
 });
 
 // app.get("*", function (req, res) {
